11ec86cf5ce810cb63ec9ee6e218d70e3c7738b1
goreleaserbot <goreleaser@carlosbecker.com>
2020-02-27 16:36:47 -0500

    Brew formula update for minidoc version v0.1.10

--- Formula/minidoc.rb
+++ Formula/minidoc.rb
@@ -2,12 +2,12 @@
 class Minidoc < Formula
   desc "mini document manager"
   homepage "https://github.com/7onetella/minidoc"
-  version "0.1.9"
+  version "0.1.10"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/7onetella/minidoc/releases/download/v0.1.9/minidoc_0.1.9_Darwin_x86_64.tar.gz"
-    sha256 "ccad5c1aa4536516e07bd66be9a403bde81fd64bd3bf98384a4e4ac9af57aa11"
+    url "https://github.com/7onetella/minidoc/releases/download/v0.1.10/minidoc_0.1.10_Darwin_x86_64.tar.gz"
+    sha256 "75451f7fcd6ee295f64a42750b02ea191d6efc4b3e10c532ccf7c6838efda3a5"
   elsif OS.linux?
   end
 
